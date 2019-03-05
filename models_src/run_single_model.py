8ff4b3502749182e3957ef9b3424f2205c054d50
Piet Clemens Grubitz <piet_clemens_grubitz@tu-dresden.de>
2019-03-05 17:35:28 +0100

    commenting recent changes

--- models_src/run_single_model.py
+++ models_src/run_single_model.py
@@ -53,37 +53,58 @@ class RunSingleModel:
         # self.value_loss = np.zeros((1, epochs))
 
     def run(self):
+        """carry out one or multiple runs of training"""
+        # create overall path for trained model and Tensorboard Graphs
         if self.comments == '':
             model_path = './saved_models/' + self.name + '/'
+
+        # create a subdirectory if comments are passed
         else:
             model_path = './saved_models/' + self.name + '/' \
                 + self.comments + '/'
-
-        train, test = self.create_training_data()
-
         os.makedirs(model_path, exist_ok=True)
         print(self.sim_title)
 
 
+        # create training and testing data
+        train, test = self.create_training_data()
+
+        # Since Multiple runs should be in a new directory each:
+        # run numbers are generated taking into account
+        # previous directories created for runs
         runs_completed = 0
         total_run_number = 1
 
+
+        # iterate until the number of runs specified have been carried out,
+        # regardless of how many runs have been carried out previousky
         while runs_completed<self.runs:
             log_dir = model_path + 'Graph/' + self.sim_title \
                                 + '/run' + str(total_run_number) + '/'
+
+        # Check whether a run number still exists
             if not os.path.isdir(log_dir):
                 print('run number: ' + str(runs_completed + 1))
                 self.run_model(train, test, log_dir)
                 runs_completed += 1
+
+        # Otherwise increment run number until a free one is found
             total_run_number += 1
 
     def run_model(self, train, test, log_dir):
+        """one training run isolated two save memory"""
+
+        # instantiate Neural Network model with parameters
         current_model = self.model(*self.params)
+
+        # Enable TensorBoard analytic files
         tbCallBack = TensorBoard(log_dir=log_dir, histogram_freq=0, write_images=True,
                                  write_graph=True)
-
+        # Training
         current_model.fit(*train, validation_data=test, epochs=self.epochs, verbose=2,
                   callbacks=[tbCallBack])
+
+        # save models and weights
         current_model.save(log_dir + self.sim_title + ".h5")
 
 
