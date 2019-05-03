9e7415aacb3c8250436cd963804535129fb784b6
Piet Clemens Grubitz <piet_clemens_grubitz@tu-dresden.de>
2019-05-03 13:57:38 +0200

    moved handling of input files to training_data.py

--- models_src/training_data.py
+++ models_src/training_data.py
@@ -1,22 +1,47 @@
+"""Doc_string"""
+
+import os
 import numpy as np
 import h5py
 
-
 class TrainingData():
-    def __init__(self, input_paths):
-        sets = []
-        for path in input_paths:
-            sets.append(self.process_input(path))
-
-        sets_array = np.concatenate(sets)
-        self.eT_scale = np.amax(sets_array)
+    def __init__(self, path='/ZIH.fast/users/ML_berthold_grubitz/data/TestCell/OFMaxFinder/',
+                 prefix='EMB_EMMiddle_0.5125X0.0125_OF_'):
+        sets = self.create_input_files(path=path, prefix=prefix) 
+        self.eT_scale = np.amax(sets)
         self.normalized = sets / self.eT_scale
+
 
     def process_input(self, input_path):
         input_file = h5py.File(input_path, 'r')
         dig_eT = input_file['sequence_dig_eT'][()]
         hit_eT = input_file['sequence_hit_eT'][()]
-        return np.column_stack((dig_eT, hit_eT))
+        OFMax_eT = input_file['sequence_OFMax_eT'][()]
+        return np.column_stack((dig_eT, hit_eT, OFMax_eT))
+
+
+    def OF_data(self, total_length):
+        OF_sets = np.asarray(self.normalized[:, :, 2])
+        OF_train, OF_test = np.split(np.asarray(OF_sets), 2, axis=1)
+        nf, samples = OF_train.shape
+        OF_train = OF_train.reshape(nf, samples, 1)
+        OF_test = OF_test.reshape(nf, samples, 1)
+
+
+        nof, nos, dim = OF_test.shape
+        new_length = int(total_length / nof)
+        OF_train = OF_train[:, -new_length:, :]
+        OF_test = OF_test[:, -new_length:, :]
+
+        OF_train = np.concatenate(OF_train)
+        OF_test = np.concatenate(OF_test)
+
+        # Reshape into 3D array for NN:
+        OF_train = OF_train.reshape(total_length, 1, 1)
+        OF_test = OF_test.reshape(total_length, 1, 1)
+
+        return OF_train, OF_test
+
 
     def delayed_td(self, n):
         (X_train, Y_train), (X_test, Y_test) = self.chunk_td()
@@ -82,3 +107,21 @@ class TrainingData():
             temp_list.append(temp_array)
 
         return np.concatenate(temp_list)
+
+
+    def create_input_files(self, path, prefix):
+        """Finding all files with a certain prefix and returning
+        respective paths in list
+        default (path='/ZIH.fast/users/ML_berthold_grubitz/data/TestCell/')
+        default (prefix='EMB_EMMiddle_0.5125X0.0125_5GeV_')
+        """
+
+        sets = []
+        for file in os.listdir(path):
+            if file.startswith(prefix):
+                file_name = os.path.join(path, file)
+                sets.append(self.process_input(file_name))
+
+        return sets
+
+
