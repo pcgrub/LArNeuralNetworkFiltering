b415eee66901159cf01beb85da9860ec8cd8079a
piet <piet@ellesmere.lan>
2019-05-06 17:43:14 +0200

    doc-strings for training_data.py

--- models_src/training_data.py
+++ models_src/training_data.py
@@ -1,101 +1,199 @@
-"""Doc_string"""
+"""
+TrainingData class
+
+This class processes the data used for training and testing.
+
+Parameters:
+path - containing files with 10000 Training samples each
+prefix - file prefix for each of these files
+
+Defaults:
+path = /ZIH.fast/users/ML_berthold_grubitz/data/TestCell/ofMaxFinder/
+prefix = EMB_EMMiddle_0.5125X0.0125_of_
+
+
+Methods:
+of_data - Returns the output the Optimal-Filter+Maxfinder would give
+          on the training and testing set
+
+delayed_td - Returns training data where the output is delayed by n samples
+
+chunk_td - Generating unprocessed training data
+
+window_dim_1_sized_td - Generates processed training data, where an input of
+                        length 'slice_len' is mapped to an output of length 1.
+                        The output gets delayed to match specifications,
+                        i.e the output samples should be given after 'delay-1'
+                        samples of the corresponding detector pulse.
+
+create_input_files - Finding all files with a certain prefix and returning
+                     data from those files.
+
+"""
 
 import os
 import numpy as np
 import h5py
 
 class TrainingData():
-    def __init__(self, path='/ZIH.fast/users/ML_berthold_grubitz/data/TestCell/OFMaxFinder/',
-                 prefix='EMB_EMMiddle_0.5125X0.0125_OF_'):
+    def __init__(self, path='/ZIH.fast/users/ML_berthold_grubitz/data/TestCell/ofMaxFinder/',
+                 prefix='EMB_EMMiddle_0.5125X0.0125_of_'):
         sets = self.create_input_files(path=path, prefix=prefix) 
         self.eT_scale = np.amax(sets)
         self.normalized = sets / self.eT_scale
 
 
     def process_input(self, input_path):
+        """
+        Extracting relevant columns from an HDF5-input file
+
+        The columns are:
+        'dig_eT' - Digitizer Output, E_T [GeV]
+        'hit_eT' - real deposited enegery, E_T [GeV]
+        'OFMax_eT' - Energy reconstruction using Optimal-Filter+Maximum-Finder,
+        E_T [GeV]
+
+        Returns:
+        numpy-ndarray containing the three clolumns
+        """
         input_file = h5py.File(input_path, 'r')
         dig_eT = input_file['sequence_dig_eT'][()]
         hit_eT = input_file['sequence_hit_eT'][()]
-        OFMax_eT = input_file['sequence_OFMax_eT'][()]
-        return np.column_stack((dig_eT, hit_eT, OFMax_eT))
+        ofMax_eT = input_file['sequence_OFMax_eT'][()]
+        return np.column_stack((dig_eT, hit_eT, ofMax_eT))
 
 
-    def OF_data(self, total_length):
-        OF_sets = np.asarray(self.normalized[:, :, 2])
-        OF_train, OF_test = np.split(np.asarray(OF_sets), 2, axis=1)
-        nf, samples = OF_train.shape
-        OF_train = OF_train.reshape(nf, samples, 1)
-        OF_test = OF_test.reshape(nf, samples, 1)
+    def of_data(self, total_length):
+        """
+        Returns the output the Optimal-Filter+Maxfinder would give
+        on the training and testing set
+
+        Parameters:
+        total_length - length of output data sets
+
+        Returns:
+        Pair training set and testing set, each being pair of input and output
+        data.
+        """
+        of_sets = np.asarray(self.normalized[:, :, 2])
+        of_train, of_test = np.split(np.asarray(of_sets), 2, axis=1)
+        nf, samples = of_train.shape
+        of_train = of_train.reshape(nf, samples, 1)
+        of_test = of_test.reshape(nf, samples, 1)
 
 
-        nof, nos, dim = OF_test.shape
+        nof = of_test.shape[0]
         new_length = int(total_length / nof)
-        OF_train = OF_train[:, -new_length:, :]
-        OF_test = OF_test[:, -new_length:, :]
+        of_train = of_train[:, -new_length:, :]
+        of_test = of_test[:, -new_length:, :]
 
-        OF_train = np.concatenate(OF_train)
-        OF_test = np.concatenate(OF_test)
+        of_train = np.concatenate(of_train)
+        of_test = np.concatenate(of_test)
 
         # Reshape into 3D array for NN:
-        OF_train = OF_train.reshape(total_length, 1, 1)
-        OF_test = OF_test.reshape(total_length, 1, 1)
+        of_train = of_train.reshape(total_length, 1, 1)
+        of_test = of_test.reshape(total_length, 1, 1)
 
-        return OF_train, OF_test
+        return of_train, of_test
 
 
     def delayed_td(self, n):
-        (X_train, Y_train), (X_test, Y_test) = self.chunk_td()
+        """
+        Returns training data where the output is delayed by n samples
 
-        Y_train = np.roll(Y_train, n, axis=1)
-        Y_test = np.roll(Y_test, n, axis=1)
+        Parameters:
+        n - number of samples the output should be delayed
 
-        Y_train[:, :n, :] = 0
-        Y_test[:, :n, :] = 0
+        Returns:
+        Pair training set and testing set, each being pair of input and output
+        data.
+        """
+        (x_train, y_train), (x_test, y_test) = self.chunk_td()
 
-        return (X_train, Y_train), (X_test, Y_test)
+        y_train = np.roll(y_train, n, axis=1)
+        y_test = np.roll(y_test, n, axis=1)
+
+        y_train[:, :n, :] = 0
+        y_test[:, :n, :] = 0
+
+        return (x_train, y_train), (x_test, y_test)
 
     def chunk_td(self):
+        """
+        Generating unprocessed training data.
+
+        Returns:
+        Pair training set and testing set, each being pair of input and output
+        data.
+        """
+
         d_norm = np.asarray(self.normalized[:, :, 0])
         h_norm = np.asarray(self.normalized[:, :, 1])
 
         # Split in half for testing and training data
-        X_train, X_test = np.split(d_norm, 2, axis=1)
-        Y_train, Y_test = np.split(h_norm, 2, axis=1)
-        nf, samples = X_train.shape
+        x_train, x_test = np.split(d_norm, 2, axis=1)
+        y_train, y_test = np.split(h_norm, 2, axis=1)
+        nf, samples = x_train.shape
 
-        X_train = X_train.reshape(nf, samples, 1)
-        X_test = X_test.reshape(nf, samples, 1)
-        Y_train = Y_train.reshape(nf, samples, 1)
-        Y_test = Y_test.reshape(nf, samples, 1)
+        x_train = x_train.reshape(nf, samples, 1)
+        x_test = x_test.reshape(nf, samples, 1)
+        y_train = y_train.reshape(nf, samples, 1)
+        y_test = y_test.reshape(nf, samples, 1)
 
-        return (X_train, Y_train), (X_test, Y_test)
+        return (x_train, y_train), (x_test, y_test)
 
     def window_dim_1_sized_td(self, slice_len, delay):
-        (X_train, Y_train), (X_test, Y_test) = self.delayed_td(delay)
+        """
+        Generates processed training data, where an input of length 'slice_len'
+        is mapped to an output of length 1. The output gets delayed to match
+        specifications, .i.e the output samples should be given after 'delay-1'
+        samples of the corresponding detector pulse.
+
+
+        Parameters:
+        delay - number of samples the output should be delayed
+        slice_len - number of samples given as input
+
+        Returns:
+        Pair training set and testing set, each being pair of input and output
+        data.
+        """
+        (x_train, y_train), (x_test, y_test) = self.delayed_td(delay)
 
         # Process input into overlapping slices with length "slice_len"
-        X_train = self.split_up(X_train, slice_len)
-        X_test = self.split_up(X_test, slice_len)
+        x_train = self.split_up(x_train, slice_len)
+        x_test = self.split_up(x_test, slice_len)
 
         # shorten corresponding output data by taking
         # samples away from the front
-        nof, nos, dim = Y_test.shape
-        total_length = len(X_test)
+        nof, nos, dim = y_test.shape
+        total_length = len(x_test)
         new_length = int(total_length / nof)
-        Y_train = Y_train[:, -new_length:, :]
-        Y_test = Y_test[:, -new_length:, :]
+        y_train = y_train[:, -new_length:, :]
+        y_test = y_test[:, -new_length:, :]
 
-        Y_train = np.concatenate(Y_train)
-        Y_test = np.concatenate(Y_test)
+        y_train = np.concatenate(y_train)
+        y_test = np.concatenate(y_test)
 
         # Reshape into 3D array for NN:
-        Y_train = Y_train.reshape(total_length, 1, 1)
-        Y_test = Y_test.reshape(total_length, 1, 1)
+        y_train = y_train.reshape(total_length, 1, 1)
+        y_test = y_test.reshape(total_length, 1, 1)
 
-        return (X_train, Y_train), (X_test, Y_test)
+        return (x_train, y_train), (x_test, y_test)
+
 
     def split_up(self, sequence, slice_len):
-        nof, nos, dim = sequence.shape
+        """
+        Generating overlapping windows
+
+        Parameters:
+        sequence - long sequence of samples
+        slice_len - size of each windows
+
+        Returns:
+        Array of overlapping windows with length 'slice_len'
+        """
+        nof, nos = sequence.shape [:-1]
         new_length = nos - slice_len + 1
 
         # split up each file and put in list
@@ -110,10 +208,17 @@ class TrainingData():
 
 
     def create_input_files(self, path, prefix):
-        """Finding all files with a certain prefix and returning
-        respective paths in list
-        default (path='/ZIH.fast/users/ML_berthold_grubitz/data/TestCell/')
-        default (prefix='EMB_EMMiddle_0.5125X0.0125_5GeV_')
+        """
+        Finding all files with a certain prefix and returning
+        data from those files
+
+        Returns:
+        list with each element being a three column matrix according to
+        'self.process_inputs'
+
+        Parameters:
+        path - containing files with 10000 Training samples each
+        prefix - file prefix for each of these files
         """
 
         sets = []
@@ -123,5 +228,3 @@ class TrainingData():
                 sets.append(self.process_input(file_name))
 
         return sets
-
-
