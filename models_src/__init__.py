ce89fb7c5c8c74fabe0a6e6d4507a53076d57855
Piet Clemens Grubitz <piet_clemens_grubitz@tu-dresden.de>
2019-05-09 11:40:44 +0200

    added new modules for pruning to __init__.py

--- models_src/__init__.py
+++ models_src/__init__.py
@@ -4,3 +4,7 @@ from .training_data import TrainingData
 from .loss_functions import *
 from .optimal_filter import *
 from .feed_forward import *
+from .weight_mask import WeightMask
+from .pruning_run import PruningRun
+from .pruning_callback import PruningCallback
+
