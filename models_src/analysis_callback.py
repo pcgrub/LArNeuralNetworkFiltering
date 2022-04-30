4e873a4e77da2fca02c811227a464267c64e6fa6
Clemens Grubitz <11209549+pcgrub@users.noreply.github.com>
2022-04-30 10:10:11 +0200

    replaced df.append with pd.concat

--- models_src/analysis_callback.py
+++ models_src/analysis_callback.py
@@ -17,13 +17,13 @@ class AnalysisCallback(Callback):
         self.testing_data = testing_data
         self.scale = scale
         self.pu_threshold = pu_threshold
-        columns = ['epoch',
+        self.columns = ['epoch',
                    'mean_sig', 'median_sig', 'std_sig',
                    '1percentile_sig', '99percentile_sig',
                    'mean_pu', 'median_pu', 'std_pu',
                    '1percentile_pu', '99percentile_pu',
                    'loss', 'validation_loss']
-        self.analysis_df = pd.DataFrame(columns=columns)
+        self.analysis_df = pd.DataFrame(columns=self.columns).set_index('epoch')
         self.save_path = save_path
 
     def on_epoch_end(self, epoch, logs=None):
@@ -35,26 +35,31 @@ class AnalysisCallback(Callback):
         diff_pu = difference[y_true<= self.pu_threshold]
 
         loss = logs.get('loss')
-        val_loss = logs.get('val_loss')
+        val_loss = logs.get('val_loss') 
 
-        self.analysis_df = self.analysis_df.append(
-            {'mean_sig' : np.mean(diff_sig),
-             'median_sig' : np.median(diff_sig),
-             'std_sig' : np.std(diff_sig),
-             '1percentile_sig' : np.percentile(diff_sig, 1),
-             '99percentile_sig' : np.percentile(diff_sig, 99),
+        data_point_df = pd.DataFrame(
+            columns=self.columns,
+            data=[{
+                'mean_sig' : np.mean(diff_sig),
+                'median_sig' : np.median(diff_sig),
+                'std_sig' : np.std(diff_sig),
+                '1percentile_sig' : np.percentile(diff_sig, 1),
+                '99percentile_sig' : np.percentile(diff_sig, 99),
 
-             'mean_pu' : np.mean(diff_pu),
-             'median_pu' : np.median(diff_pu),
-             'std_pu' : np.std(diff_pu),
-             '1percentile_pu' : np.percentile(diff_pu, 1),
-             '99percentile_pu' : np.percentile(diff_pu, 99),
+                'mean_pu' : np.mean(diff_pu),
+                'median_pu' : np.median(diff_pu),
+                'std_pu' : np.std(diff_pu),
+                '1percentile_pu' : np.percentile(diff_pu, 1),
+                '99percentile_pu' : np.percentile(diff_pu, 99),
 
-             'epoch': epoch,
-             'loss': loss,
-             'validation_loss': val_loss
-
-             }, ignore_index=True)
+                'epoch': epoch,
+                'loss': loss,
+                'validation_loss': val_loss,
+                }],
+             )
+        
+        data_point_df.set_index('epoch', inplace=True)
+        self.analysis_df = pd.concat([self.analysis_df, data_point_df], join='outer', axis=0, copy=False) 
 
 
     def on_train_end(self ,logs={}):
